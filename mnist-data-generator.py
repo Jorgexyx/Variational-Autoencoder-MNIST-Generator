79d0465726875a5e83159cf66c26027a2943f33f
jorgexyx <bautisjo@sonoma.edu>
2020-11-17 22:36:09 -0800

    Add VAE model with decoder

--- mnist-data-generator.py
+++ mnist-data-generator.py
@@ -5,19 +5,35 @@ from keras import objectives
 from keras.datasets import mnist
 import numpy as np
 
+def vae_loss(x, x_decoded_mean, z_log_var, z_mean, original_dim=784):
+    xent_loss = original_dim * objectives.binary_crossentropy(x, x_decoded_mean)
+    k1_loss = -0.4 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+    return xent_loss + k1_loss
+
 def sampling(args):
     z_mean, z_var = args
     epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0,)
     return z_mean + K.exp(z_var / 2) * epsilon
 
-def get_encoder(img_dimensions, intermediate_dimensions, latent_dim):
+def get_decoder():
+    """
+    Decoder to convert from latent space into new image
+    """
+    input_node = Input(shape=(latent_dim,), name="input_decoder")
+    decoder_h = Dense(intermediate_dimensions, activation='relu', name='decoder_h')(input_node)
+
+    decoder_node = Dense(img_dimensions, activation='sigmoid', name="deocder_node")(decoder_h)
+    decoder = Model(input_node, decoder_node, name='deocder_model')
+    return decoder
+
+
+def get_encoder():
     """
     Encoder an mnist image and define its latent space (z) mean and variance
     Returns:
         Encoder that defines a latent space (z) mean and variance
 
     """
-    input_node = Input(shape=(img_dimensions,), name="input")
     intermediate_node = Dense(intermediate_dimensions, activation='relu', name="encoding")(input_node)
 
     # define latent space
@@ -35,5 +51,14 @@ intermediate_dimensions = 256
 latent_dim = 2
 batch_size = 100
 
-get_encoder(img_dimensions, intermediate_dimensions, latent_dim)
+"""
+Define VAE model
+"""
+input_node = Input(shape=(img_dimensions,), name="input_encoder")
+encoder = get_encoder()
+decoder = get_decoder()
+output_combined = decoder(encoder(input_node)[2])
+vae = Model(input_node, output_combined)
+vae.summary()
+vae.compile(optimizer='rmsprop', loss=vae_loss)
 
